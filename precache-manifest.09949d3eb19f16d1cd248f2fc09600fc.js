5021825d4f565d3e1352ae46a379f4c9baa47356
emfree1 <emfree20@gmail.com>
2020-09-22 19:20:40 +0300

    Updates

---
+++ precache-manifest.09949d3eb19f16d1cd248f2fc09600fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40c61fdc05bf3f58dfcf323f63d0828",
+    "url": "/react-git-card/index.html"
+  },
+  {
+    "revision": "09ab36029e0a9304442b",
+    "url": "/react-git-card/static/css/main.6d7f5595.chunk.css"
+  },
+  {
+    "revision": "3da532f1d84250a6576a",
+    "url": "/react-git-card/static/js/2.3a1b3f9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-git-card/static/js/2.3a1b3f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ab36029e0a9304442b",
+    "url": "/react-git-card/static/js/main.922bd391.chunk.js"
+  },
+  {
+    "revision": "7d138ccc4fed4b16ad59",
+    "url": "/react-git-card/static/js/runtime-main.5f16921e.js"
+  }
+]);
